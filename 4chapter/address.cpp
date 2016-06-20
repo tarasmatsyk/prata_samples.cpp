56a4189667123952ce8ccf7f36d4abf89f2b41f7
tarasmatsyk <tamatsyk@gmail.com>
2016-06-20 09:27:02 +0300

    add addresses example

---
+++ 4chapter/address.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+  using namespace std;
+  int donuts = 6;
+  double cups = 4.5;
+
+  cout << "dounts value = " << donuts;
+  cout << " and donuts address = " << &donuts << endl;
+
+  cout << "cups value = " << cups;
+  cout << " and cups address = " << &cups << endl;
+}
