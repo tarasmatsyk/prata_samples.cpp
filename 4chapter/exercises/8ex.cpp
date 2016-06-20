2ef4cd982e97e25d9eb43a71b3eaab419a93f1f3
tarasmatsyk <tamatsyk@gmail.com>
2016-06-20 23:08:32 +0300

    complete 8th excersice

---
+++ 4chapter/exercises/8ex.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+struct fucking_pizza
+{
+  string fucking_name;
+  int diameter;
+  int weight; 
+};
+
+int main()
+{
+  fucking_pizza *pizza = new fucking_pizza;
+
+  cout << "Gimme a diameter for that awesome pizza: " << endl;
+  cin >> pizza->diameter;
+  cout << "Give me the name of your awesome pizza: " << endl;
+  cin.get();
+  getline(cin, pizza->fucking_name);
+  cout << "Gimme a weight for that awesome pizza: " << endl;
+  cin >> pizza->weight;
+
+  cout << "Pizza info: \n";
+  cout << "Name: " << pizza->fucking_name << endl;
+  cout << "Diameter: " << pizza->diameter << endl;
+  cout << "Weight: " << pizza->weight << endl;
+
+  cin.get();
+  return 0;
+}
